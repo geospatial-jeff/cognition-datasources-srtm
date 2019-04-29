9e9cd977ea920ff4ca6d0e98599e5cf175c91892
Jeff Albrecht <geospatialjeff@gmail.com>
2019-04-28 18:33:35 -0700

    Delete handler.py

--- handler.py
+++
@@ -1,7 +0,0 @@
-from datasources import Manifest
-
-def SRTM(event, context):
-    manifest = Manifest()
-    manifest['SRTM'].search(**event)
-    response = manifest.execute()
-    return response
